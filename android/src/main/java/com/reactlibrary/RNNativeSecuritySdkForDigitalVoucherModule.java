a40a2f8e27b876a71f8c0994171e64f8cad21c95
zorro199696@gmail.com <mac@macui-MacBook-Pro.local>
2020-02-22 19:16:33 +0900

    added secure wallet arr

--- android/src/main/java/com/reactlibrary/RNNativeSecuritySdkForDigitalVoucherModule.java
+++ android/src/main/java/com/reactlibrary/RNNativeSecuritySdkForDigitalVoucherModule.java
@@ -1,6 +1,9 @@
 
 package com.reactlibrary;
 
+import android.content.Context;
+import android.widget.Toast;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
